ffa0268579e728348880c4c3d230a9dce8d66d66
Leosoft <leosoft3031@gmail.com>
2022-05-11 18:20:07 -0300

    New article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,32 @@
 class ArticlesController < ApplicationController
   def index
-    #variable de instancia
+    # variable de instancia
     @articles = Article.all
   end
 
   # Show
   def show
     @article = Article.find(params[:id])
+  end
+
+  # New
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end

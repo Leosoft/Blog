150884464c9e410ace87bf16c3c8a8e3fa2c5a99
Leosoft <leosoft3031@gmail.com>
2022-05-11 17:48:34 -0300

    Show Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
   def index
+    #variable de instancia
+    @articles = Article.all
+  end
+
+  # Show
+  def show
+    @article = Article.find(params[:id])
   end
 end
